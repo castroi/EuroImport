286fb39c54174e3b3d66f2e1e59180fc0b14168b
castroi <idan.castro@gmail.com>
2019-02-21 22:13:24 +0200

    Security problem

--- EuroImport/JsonReader.cs
+++ EuroImport/JsonReader.cs
@@ -13,6 +13,7 @@ namespace EuroImport
         {
             using (WebClient wc = new WebClient { Encoding = Encoding.UTF8})
             {
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)768 | (SecurityProtocolType)3072;
                 var json = wc.DownloadString(url);
                 return JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
             }
